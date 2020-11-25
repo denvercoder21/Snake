0709898055f8f0e200359e18c92f322cd87a8023
denvercoder21 <github@ottair.net>
2020-11-25 08:57:00 +0100

    Remove game_viewmodel

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include "model/game.h"
 #include "model/snake.h"
 #include "viewmodel/board_viewmodel.h"
-#include "viewmodel/game_viewmodel.h"
 #include "utils.h"
 
 #include <QGuiApplication>
@@ -13,21 +12,20 @@ int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
-    QGuiApplication app(argc, argv);    auto board_logic = board{definitions::board_width, definitions::board_height};
+    QGuiApplication app(argc, argv);
 
     qRegisterMetaType<position>("position");
     qmlRegisterUncreatableType<board_viewmodel>("Board.CellState", 1, 0, "CellState", "Foo");
 
-    auto board_view = board_viewmodel{board_logic};
-
+    auto board_logic = board{definitions::board_width, definitions::board_height};
     auto snake_logic = snake{snake::direction::down};
-
     auto game_logic = game{board_logic, snake_logic};
-    auto game_view = game_viewmodel{game_logic};
+
+    auto board_view = board_viewmodel{board_logic};
 
     QQmlApplicationEngine engine{};
 
-    engine.rootContext()->setContextProperty("Game", &game_view);
+    engine.rootContext()->setContextProperty("Game", &game_logic);
     engine.rootContext()->setContextProperty("BoardModel", &board_view);
 
     engine.load("qrc:/main.qml");
